fccdc9e2ab8680cff3fdc70d30a97aab14b098ef
cnzhihua <cnzhihua@gmail.com>
2018-03-28 18:17:13 +0800

    remove unused class and update class description

--- src/Charts.php
+++ src/Charts.php
@@ -8,45 +8,52 @@
 namespace hzhihua\frappe;
 
 use Yii;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Json;
-use yii\web\JsExpression;
 use yii\web\View;
+use yii\web\JsExpression;
 use yii\base\Widget;
 use yii\base\InvalidConfigException;
 
 /**
- * 生成图形统计图
+ * 安装：
  * ```php
- * <?= Charts::widget([
+ * composer require hzhihua/yii2-frappe-charts
+ * ```
+ * 
+ * 生成图形统计图，在视图模板中调用：
+ * ```php
+ * <? \hzhihua\frappe\Charts::widget([
+ *     'view' => 'yii\web\View', // or $this, your customize view class/object, it must be extends [[yii\web\View]], default Yii::$app->getView()
+ *     'instance_var_name' => 'chart', // var chart = new Chart({});
+ *     'instance_var_name_keyword' => 'var', // var chart = new Chart({});
+ *     'asset' => 'hzhihua\\frappe\\ChartsAsset', // asset source class
+ *     'selector' => '#chart',
  *     'config' => [
- *         'parent' => '#chart',
  *         'height' => 250,
  *         'title' => "My Chart",
- *         'type' => 'bar',
+ *         'type' => 'bar', // 'axis-mixed' or 'bar', 'line', 'pie', 'percentage'
  *         'colors' => ['#7cd6fd', '#743ee2'],
- *         'format_tooltip_x' => '<%d => (d +  * \'\').toUpperCase()%>',
- *     ],
- *     'data' => [
- *         'labels' => ["12am-3am", "3am-6pm",  * "6am-9am", "9am-12am", "12pm-3pm", "3pm-6pm",  * "6pm-9pm", "9am-12am"
- *         ],
- *         'datasets' => [
- *             [
- *                 'title' => "Some Data",
- *                 'values' => [25, 40, 30, 35, 8, 52,  * 17, -4],
+ *         'format_tooltip_x' => new \yii\web\JsExpression('d => (d + \'\').toUpperCase()'), // 将生成原生js代码 d => (d + '').toUpperCase()
+ *         'data' => [
+ *             'labels' => ["12am-3am", "3am-6pm", "6am-9am", "9am-12am", "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"
  *             ],
- *             [
- *                 'title' => "Another Set",
- *                 'values' => [25, 50, -10, 15, 18, 32,  * 27, 14],
+ *             'datasets' => [
+ *                 [
+ *                     'name' => "Some Data",
+ *                     'axisPosition' => 'right',
+ *                     'values' => [25, 40, 30, 35, 8, 52, 17, -4],
+ *                 ],
+ *                 [
+ *                     'name' => "Another Set",
+ *                     'chartType' => 'line',
+ *                     'values' => [25, 50, -10, 15, 18, 32, 27, 14],
+ *                 ],
  *             ],
  *         ],
  *     ],
- *     'other' => <<<'JS'
- * chart.show_sums();
- * chart.show_averages();
- * JS
- * ]);
- * ?>
+ *     'other' => new \yii\web\JsExpression('console.log(chart)'),
+ *     // @see https://frappe.github.io/charts/
+ *     // @see \hzhihua\frappe\Charts for detail configuration info
+ * ]);?>
  * ```
  *
  * @package hzhihua\frappe
